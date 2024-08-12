c3a485d67a5b51ce8122eef806fa1ed968ffae95
mahmoud selim <m@gmail.com>
2024-08-12 07:33:01 +0300

    test subCategories & brand api

--- src/app/main/brand/brand.component.ts
+++ src/app/main/brand/brand.component.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.css']
+  styleUrls: ['./brand.component.css'],
 })
 export class BrandComponent {
-
+  allData!: any[];
+  show() {
+    this.http
+      .get('https://ecommerce.routemisr.com/api/v1/brands')
+      .subscribe((data: any) => {
+        console.log(data.data);
+        this.allData = data.data;
+      });
+  }
+  constructor(private http: HttpClient) {
+    this.show();
+  }
 }
--- src/app/main/sub-categories/sub-categories.component.ts
+++ src/app/main/sub-categories/sub-categories.component.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sub-categories',
   templateUrl: './sub-categories.component.html',
-  styleUrls: ['./sub-categories.component.css']
+  styleUrls: ['./sub-categories.component.css'],
 })
 export class SubCategoriesComponent {
-
+  allData!: any[];
+  show() {
+    this.http
+      .get('https://ecommerce.routemisr.com/api/v1/subcategories')
+      .subscribe((data: any) => {
+        console.log(data.data);
+        this.allData = data.data;
+      });
+  }
+  constructor(private http: HttpClient) {
+    this.show();
+  }
 }
